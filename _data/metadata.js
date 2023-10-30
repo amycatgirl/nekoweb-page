e7b41fc571c408a690f850f78292ad64fd09c54b
Amy <amycatgirlnya@outlook.com>
2023-10-30 11:19:33 -0400

    feat: use 11ty

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Amy",
+	url: "https://amycatgirl.codeberg.page/",
+	language: "en",
+	description: "Developer, designer and full-time catgirl",
+	author: {
+		name: "Amy Perez",
+		email: "amycatgirlnya@example.com",
+		url: "https://amycatgirl.codeberg.page/"
+	}
+}
